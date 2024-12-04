8d8c0dd4f055789db1dfd8f5c8cbc2965bf69efe
zain171m <zain171m@gmail.com>
2024-12-04 13:11:01 +0500

    deployment done

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/BenefactionPlatform-EVM-Frontend/",
+  base: "/Bene-FundRaising-EVM-Frontend/",
 });
